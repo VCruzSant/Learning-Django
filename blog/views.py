a6eaa4bcfc6b9ae6fea1f7c52d47da49b3cd9a98
VCruzSant <sviniantiago@gmail.com>
2023-10-24 15:13:32 -0300

    Context & Static files

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,18 @@ from django.shortcuts import render
 
 
 # Create your views here.
+
+
 def blog(request):
     print('index')
+
+    context = {
+        'text': 'This is Blog',
+        'title': 'Blog'
+    }
+
     return render(
         request,
-        'blog/index.html'
+        'blog/index.html',
+        context
     )
--- home/views.py
+++ home/views.py
@@ -2,9 +2,18 @@ from django.shortcuts import render
 
 
 # Create your views here.
+
+
 def home(request):
     print('index')
+
+    context = {
+        'text': 'This is Home',
+        'title': 'Home'
+    }
+
     return render(
         request,
-        'home/index.html'
+        'home/index.html',
+        context
     )
